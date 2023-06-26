ee8105d7281b7b76f98f7eb34896b0b73bff58da
mvellayan <muthu.vellayan@gmail.com>
2023-06-26 09:30:12 -0400

    work in progress

--- sql/code/p_model_close.sql
+++ sql/code/p_model_close.sql
@@ -3,7 +3,7 @@ DROP PROCEDURE IF EXISTS `model_close`;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `model_close`(in p_model_id int)
 BEGIN
 	 declare l_op_tv, l_op_iv, l_cl_tv, l_cl_iv double;
-	 declare l_con_id int;
+	 declare l_con_id, l_run_id int;
      select op_iv, op_tv, cl_iv, cl_tv into l_op_iv, l_op_tv, l_cl_iv,  l_cl_tv from model where model_id = p_model_id;
      truncate table logs;
     
@@ -12,15 +12,16 @@ BEGIN
         declare l_op_oq_id int;
         declare l_op_oq_date datetime;
 	    declare cl_p1 cursor for 
-			select r.op_oq_id, oq.quote_date, oq.con_id
-			from model_run r, option_quote oq
-			where r.model_id = p_model_id
-			and oq.id = r.op_oq_id
-			and r.cl_oq_id is null;
+			select mr.op_oq_id, oq.quote_date, oq.con_id, mr.run_id
+			from model_exp_run mr, model_exp me, option_quote oq
+			where me.model_id = p_model_id
+            and mr.run_id = me.run_id
+			and oq.id = mr.op_oq_id
+			and mr.cl_oq_id is null;
  		DECLARE CONTINUE HANDLER FOR NOT FOUND SET cl_p1_flag = TRUE;
         open cl_p1;
 		cl_p1_loop: LOOP
-        FETCH cl_p1 INTO l_op_oq_id, l_op_oq_date, l_con_id;
+        FETCH cl_p1 INTO l_op_oq_id, l_op_oq_date, l_con_id, l_run_id;
             IF cl_p1_flag THEN 
                 close cl_p1;
 				LEAVE cl_p1_loop; 
@@ -29,16 +30,17 @@ BEGIN
             -- BLOCK2 ----------------------------------------
 			BLOCK2: BEGIN
 				declare cl_p2_flag int default 0;
-				declare l_cl_oq_id, l_op_days, l_cl_days int;
+				declare l_cl_oq_id, l_op_days, l_cl_days int default 0;
                 declare l_net double(9,4);
 				declare cl_p2 cursor for 
 						select oq.id
-						from stock_quote sq, option_quote oq
+						from stock_quote sq, option_quote oq, model m
 						where oq.quote_date = sq.quote_date
+						and m.model_id = p_model_id
 						and oq.quote_date > l_op_oq_date
-                        and oq.con_id = l_con_id
-						and oq.close_tv <= ifnull(l_cl_tv, oq.close_tv)
-						and oq.iv <= ifnull(l_cl_iv, oq.iv)
+						and oq.con_id = l_con_id
+						and oq.close_tv <= ifnull(m.cl_tv, oq.close_tv)
+						and oq.iv <= ifnull(m.cl_iv, oq.iv)
 						order by sq.quote_date;
 				declare continue handler for not found set cl_p2_flag = true;
 				open cl_p2; 
@@ -46,14 +48,21 @@ BEGIN
 				cl_p2_loop: LOOP
 				fetch from cl_p2 INTO l_cl_oq_id;
 					if cl_p2_flag THEN
-                      update model_run set cl_oq_id =0, reason="Expired" where model_id = p_model_id and op_oq_id = l_op_oq_id;
+                      select id into l_cl_oq_id from option_quote oq where con_id in (
+							select con_id from option_quote oq where id = l_op_oq_id)
+					  order by quote_date desc limit 1;                      
+					  call get_net(l_op_oq_id, l_cl_oq_id, l_net, l_op_days, l_cl_days);
+					  update model_exp_run 
+						   set cl_oq_id=l_cl_oq_id, net = l_net, op_days = l_op_days, cl_days = l_cl_days, reason="Expired."
+					  where run_id = l_run_id and op_oq_id = l_op_oq_id;
 					  close cl_p2;
 					  leave cl_p2_loop;
 					end if;
+                    
                     call get_net(l_op_oq_id, l_cl_oq_id, l_net, l_op_days, l_cl_days);
-                    update model_run 
+                    update model_exp_run 
                        set cl_oq_id=l_cl_oq_id, net = l_net, op_days = l_op_days, cl_days = l_cl_days, reason="Found Exit"
-					where model_id = p_model_id and op_oq_id = l_op_oq_id;
+					where run_id = l_run_id and op_oq_id = l_op_oq_id;
                     close cl_p2;
 					leave cl_p2_loop;
 				end loop cl_p2_loop;
--- sql/code/p_model_open.sql
+++ sql/code/p_model_open.sql
@@ -2,41 +2,50 @@ DELIMITER $$
 DROP PROCEDURE IF EXISTS `model_open`;DROP PROCEDURE IF EXISTS `model_open`;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `model_open`(in p_model_id int)
 BEGIN
-     declare l_expiry date;
-	 declare l_op_tv double;
-	 declare l_op_iv double;
-	 declare l_cl_tv double;
-	 declare l_cl_iv double;
-
+     declare l_sample_per_hr int;
 
      declare op cursor for 
-		select oq.id
-		from option_quote oq, option_list ol, stock_quote sq 
-		where oq.con_id = ol.con_id 
-        and ol.expiry=l_expiry
-        and oq.id not in (select oq.id from model_run r where r.model_id = p_model_id)
-		and oq.quote_date = sq.quote_date
-		and option_type = 'C'
-        and oq.open_tv >= ifnull(l_op_tv, oq.open_tv)
-        and oq.iv >= ifnull(l_op_iv, oq.iv)
-		order by sq.quote_date;
+        select DATE_FORMAT(sq.quote_date,'%Y-%m-%d %H'), me.run_id, count(oq.id), SUBSTRING(group_concat(oq.id order by rand()), 1, 10*m.sample_per_hr)
+        from option_quote oq, option_list ol, stock_quote sq, model_exp me, model m
+        where m.model_id = me.model_id 
+        and oq.con_id = ol.con_id 
+        and ol.expiry=me.expiry
+        and oq.id not in (select oq.id from model_exp_run r where r.run_id = me.run_id)
+        and oq.quote_date = sq.quote_date
+        and option_type = 'C'
+        and oq.open_tv >= ifnull(m.op_tv, oq.open_tv)
+        and oq.iv >= ifnull(m.op_iv, oq.iv)
+        and m.model_id = p_model_id
+        group by DATE_FORMAT(sq.quote_date,'%Y-%m-%d %H'), me.run_id
+        order by DATE_FORMAT(sq.quote_date,'%Y-%m-%d %H');
+        
 
-    select expiry, op_iv, op_tv, cl_iv, cl_tv into l_expiry, l_op_iv, l_op_tv, l_cl_iv,  l_cl_tv from model where model_id = p_model_id;
+    select sample_per_hr into l_sample_per_hr from model where model_id = p_model_id;
 
-    truncate table logs;
-    insert into logs (log) values ( CONCAT_WS(" ", l_expiry, l_op_iv, l_op_tv, l_cl_iv,  l_cl_tv));
-    
+    truncate table logs;    
     open op;
     begin
-		declare exit_flag int default 0;
-        declare l_oq_id int;
-		DECLARE CONTINUE HANDLER FOR SQLSTATE '02000' SET exit_flag = 1;
-		opLoop: LOOP
-        FETCH op INTO l_oq_id;
+        declare exit_flag int default 0;
+        declare l_quote_hr date;
+        declare l_me_run_id int;
+        declare l_quote_hr_count int;
+        declare l_open_ids varchar(2048);
+        declare l_loop_ctr INT default 0;
+
+        DECLARE CONTINUE HANDLER FOR SQLSTATE '02000' SET exit_flag = 1;
+        opLoop: LOOP
+        FETCH op INTO l_quote_hr, l_me_run_id, l_quote_hr_count, l_open_ids;
             IF exit_flag THEN LEAVE opLoop; 
             END IF;
-            # insert into logs (log) values (l_oq_id);
-            INSERT INTO model_run (model_id, op_oq_id) VALUES (p_model_id, l_oq_id);
+              insert into logs (log) values ( CONCAT_WS(" ", l_quote_hr, l_quote_hr_count, l_open_ids));
+              
+              SET l_loop_ctr = 1;
+              WHILE l_loop_ctr <= l_sample_per_hr DO
+                INSERT IGNORE INTO model_exp_run (run_id, op_oq_id) VALUES (l_me_run_id, 
+                        SUBSTRING_INDEX(SUBSTRING_INDEX(l_open_ids, ',',l_loop_ctr),',',-1));
+                SET l_loop_ctr = l_loop_ctr + 1;
+              END WHILE;
+            
         end loop;
     end;
     close op;
